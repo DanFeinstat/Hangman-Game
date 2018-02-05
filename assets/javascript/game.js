4fd4c43382653fd7408e10843bf711ec5f9e2bb3
DanFein <danfeinstat@gmail.com>
2018-02-04 16:50:54 -0800

    added win condition and disallowed repeat guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,31 @@
-
 let locationNames = ["aukland", "salvador", "stuttgart", "johannesburg", "philadelphia"]
-let word = locationNames[Math.floor(Math.random()*locationNames.length)];
+let word;
 let answerArray = [];
 let lettersUsed = [];
-let guessLeft = 10;
-let remainingLetters = word.length;
+let guessLeft = 15;
+let winCounter = 0;
+let remainingLetters = 1;
+
+//Chooses the Location at random
+function getLocation(){
+  word = locationNames[Math.floor(Math.random()*locationNames.length)];
+  remainingLetters = word.length;
+}
+getLocation();
+
 console.log(locationNames);
 console.log(word);
-console.log(guessLeft.toString());
+console.log(remainingLetters);
 
 //Sets blanks equal to the word to guess
 //Sets chances left
-
 function generatePuzzle(){
   for (var i = 0; i < word.length; i++){
     answerArray[i]="_";
   };
   document.getElementById("word").innerHTML = answerArray.join("");
   document.getElementById("chances-left").innerHTML = guessLeft.toString();
+  document.getElementById("letters-used").innerHTML = "";
 };
 generatePuzzle(word);
 
@@ -29,6 +37,12 @@ document.onkeyup = function(event) {
   var charCode = event.keyCode;
   if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 8){
     var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    //disallow repeat guesses
+    for (var j = 0; j < lettersUsed.length; j++){
+      if (letter === lettersUsed[j]){
+        return;
+      }
+    }
     //updates letters tried
     lettersUsed.push(letter);
     document.getElementById("letters-used").innerHTML = lettersUsed.join("");
@@ -38,22 +52,34 @@ document.onkeyup = function(event) {
       if (word[k] === letter ){
         answerArray[k] = letter;
         remainingLetters--;
-      }
+        console.log("remaining letters: " + remainingLetters)
+      } //close if statement
       document.getElementById("word").innerHTML = answerArray.join("");
-    };
+    }; //close forloop
 
     //updates the word and remaining guesses
     guessLeft--;
     document.getElementById("chances-left").innerHTML = guessLeft.toString();
     console.log(guessLeft);
+
+    //win condition and reset
+    if (remainingLetters <= 0){
+      winCounter++;
+      document.getElementById("wins").innerHTML = winCounter.toString();
+      alert("You found her! She was in " + word +"!");
+      answerArray = [];
+      lettersUsed = [];
+      guessLeft = 15;
+      getLocation();
+      generatePuzzle(word);
+    };
   }
 }
-if (guessLeft > 0 || remaingingLetters > 0){
-  //do the game
-} else if (remainingLetters <= 0){
+if (remainingLetters <= 0){
   //you win!
   //reset the game
-} else if (guessLeft <= 0){
+}
+if (guessLeft <= 0){
   //She got away for now! Try again!
   //reset the game
 }
